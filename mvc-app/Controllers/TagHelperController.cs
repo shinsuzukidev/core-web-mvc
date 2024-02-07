66ec06c5b87023a60503db9520809f1df94c25f3
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-02-07 15:07:20 +0900

    Nlog - NLog.Web.AspNetCore

--- mvc-app/Controllers/TagHelperController.cs
+++ mvc-app/Controllers/TagHelperController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using mvc_app.Models.TagHelper;
+using Microsoft.AspNetCore.Http;
+using mvc_app.Extensions;
+using mvc_app.Utils;
 
 namespace mvc_app.Controllers
 {
@@ -7,6 +10,12 @@ namespace mvc_app.Controllers
     {
         public IActionResult Index()
         {
+            Logger.Trace("TagHelperController - Index");
+            Logger.Info("TagHelperController - Index");
+            Logger.Warn("TagHelperController - Index");
+            Logger.Error("TagHelperController - Index");
+            Logger.Fatal("TagHelperController - Index");
+
             var model = new RegisterViewModel();
             return View("Index", model);
         }
@@ -16,7 +25,21 @@ namespace mvc_app.Controllers
         {
             if (ModelState.IsValid)
             {
+                // tempData
                 TempData["TestData"] = "test-temp-data";
+
+                // sessionを保存(jsonとして保存）
+                var sessionKey = "registmodel";
+                HttpContext.Session.Set<RegisterViewModel>(sessionKey, model);
+                // sessionを取得
+                //if (HttpContext.Session.Get<RegisterViewModel>(sessionKey) != null)
+                if (HttpContext.Session.Get<RegisterViewModel>(sessionKey) != default)
+                {
+                    var regmodel = HttpContext.Session.Get<RegisterViewModel>(sessionKey);
+                }
+
+                HttpContext.Session.Clear();
+
 
                 // 検証がOKならMyPostMessage.cshtmlを表示
                 return View("MyPostMessage", model);
@@ -27,3 +50,4 @@ namespace mvc_app.Controllers
         }
     }
 }
+
---
+++ mvc-app/Utils/Logger.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+
+namespace mvc_app.Utils
+{
+    public static class Logger
+    {
+        private static NLog.ILogger _logger = LogManager.GetCurrentClassLogger();
+
+
+        static Logger()
+        {
+            LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+        }
+
+        public static void Trace(string message)
+        {
+            _logger.Trace(message);
+        }
+
+        public static void Info(string msg)
+        {
+            _logger.Info(msg);
+        }
+
+        public static void Warn(string msg)
+        {
+            _logger.Warn(msg);
+        }
+
+        public static void Error(string msg)
+        {
+            _logger.Error(msg);
+        }
+
+        public static void Fatal (string msg)
+        {
+            _logger.Fatal(msg);
+        }
+
+    }
+
+}
